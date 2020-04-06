2aeabd083cb3eba645c19d105e926b206df253e6
Vianney Bouault <vbouault@hotmail.fr>
2020-04-06 10:03:41 +0200

    state

--- src/compenents/Contact.js
+++ src/compenents/Contact.js
@@ -2,19 +2,28 @@ import React from 'react';
 import './Contact.css'
 import PropTypes from 'prop-types';
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-        <img className="avatar" src={props.avatar}/>
+class Contact extends React.Component {
+  state = {
+    online : this.props.online
+  }
+  handleChangeStatus = () =>{
+    const newStatus = !this.state.online;
+    this.setState({ online: newStatus });
+  }
+  render () {
+    return (
+      <div className="Contact">
+        <img className="avatar" src={this.props.avatar}/>
         <div>
-            <p className="name">{props.name}</p>
-            <div className="status">
-                <div className={props.online ? "status-online" : "status-offline"}></div>
-                <p className="status-text">{props.online ? "online" : "offline"}</p>
-            </div>
+          <p className="name">{this.props.name}</p>
+          <div className="status" onClick={this.handleChangeStatus}>
+              <div className={this.state.online ? "status-online" : "status-offline"}></div>
+              <p className="status-text">{this.state.online ? "online" : "offline"}</p>
+          </div>
         </div>
-    </div>
-  );
+      </div>
+    )
+  };
 }
 
 Contact.propTypes = {
